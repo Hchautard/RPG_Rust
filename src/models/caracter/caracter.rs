63da721466a944127af06efbb74e4a12f50f4708
JulienJAMME-ceri <julien.jamme@alumni.univ-avignon.fr>
2025-04-06 12:25:41 +0200

    Chargement des JSON et Sauvegarde d'une partis via un JSON (exemple dans le main.rs, commenté)

--- src/models/caracter/caracter.rs
+++ src/models/caracter/caracter.rs
@@ -1,7 +1,7 @@
 use serde_derive::{Deserialize, Serialize};
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Caracter {
     pub name: String,
     pub style: String,
--- src/services/json_loader.rs
+++ src/services/json_loader.rs
@@ -1,10 +1,16 @@
 use std::fs::File;
 use std::io::Read;
+use std::io::Write;
 use serde_json;
 
 use crate::models::aptitude::Aptitude;
 use crate::models::badge::Badge;
+use crate::models::caracter::client::Client;
 use crate::models::caracter::trader::Trader;
+use crate::models::arena::Arena;
+use crate::models::caracter::bouncer::Bouncer;
+use crate::models::caracter::master::Master;
+use crate::models::caracter::player::Player;
 
 pub struct JsonLoader {
 }
@@ -23,17 +29,40 @@ impl JsonLoader {
         Ok(badges)
     }
 
-    pub fn loadJsonMasters(){
+    pub fn loadJsonMasters(file_path: &str)  -> Result<Vec<Master>, Box<dyn std::error::Error>> {
+        let mut file = File::open(file_path)?;
+        let mut data = String::new();
+        file.read_to_string(&mut data)?;
+
+        let masters: Vec<Master> = serde_json::from_str(&data)?;
+        Ok(masters)
     }
 
-    pub fn loadJsonArena(){
+    pub fn loadJsonArena(file_path: &str) -> Result<Vec<Arena>, Box<dyn std::error::Error>>{
+        let mut file = File::open(file_path)?;
+        let mut data = String::new();
+        file.read_to_string(&mut data)?;
+
+        let arena: Vec<Arena> = serde_json::from_str(&data)?;
+        Ok(arena)
     }
-    pub fn loadJsonBouncers(){
-    } 
+    pub fn loadJsonBouncers(file_path: &str) -> Result<Vec<Bouncer>, Box<dyn std::error::Error>> {
+        let mut file = File::open(file_path)?;
+        let mut data = String::new();
+        file.read_to_string(&mut data)?;
+    
+        let bouncers: Vec<Bouncer> = serde_json::from_str(&data)?;
+        Ok(bouncers)
+    }
 
-    pub fn loadJsonClients(){
+    pub fn loadJsonClients(file_path: &str) -> Result<Vec<Client>, Box<dyn std::error::Error>> {
+        let mut file = File::open(file_path)?;
+        let mut data = String::new();
+        file.read_to_string(&mut data)?;
 
-    }  
+        let clients: Vec<Client> = serde_json::from_str(&data)?;
+        Ok(clients)
+    }
     pub fn loadJsonTraders(file_path: &str) -> Result<Vec<Trader>, Box<dyn std::error::Error>> {
         let mut file = File::open(file_path)?;
         let mut data = String::new();
@@ -50,6 +79,31 @@ impl JsonLoader {
 
         let aptitudes: Vec<Aptitude> = serde_json::from_str(&data)?;
         Ok(aptitudes)
+    }
+
+    pub fn save_player_to_json(file_path: &str, player: &Player) -> Result<(), Box<dyn std::error::Error>> {
+        let mut players: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+    
+        // Lecture du fichier existant
+        if let Ok(mut file) = File::open(file_path) {
+            let mut data = String::new();
+            file.read_to_string(&mut data)?;
+            if !data.trim().is_empty() {
+                // Désérialisation du fichier JSON en une Map
+                players = serde_json::from_str(&data)?;
+            }
+        }
+    
+        let player_level = player.level.to_string();
+    
+        let player_json = serde_json::to_value(player)?;
+        players.insert(player_level, player_json);
+    
+        let json = serde_json::to_string_pretty(&players)?;
+        let mut file = File::create(file_path)?;
+        file.write_all(json.as_bytes())?;
+    
+        Ok(())
     }
 
 }
